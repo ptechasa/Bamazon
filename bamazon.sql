5e9989d51cc788740a700b51c8830628eef1b519
miko <ptechasa@mail.ccsf.edu>
2019-09-19 23:57:49 -0700

    created connection to SQL and hiding the credential

--- bamazon.sql
+++ bamazon.sql
@@ -7,7 +7,7 @@ CREATE TABLE products (
 item_id  INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(255),
 department_name VARCHAR(255),
-price DECIMAL (8,2),
+price DECIMAL (8,2), 
 stock_quantity INT NOT NULL,
 PRIMARY KEY(item_id)
 );
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,32 @@
 var mysql = require('mysql');
+
+// Load the NPM Package inquirer
+var inquirer = require("inquirer");
+
 const keys = require('./keys.js');
-var stuff = require('dotenv').config()
-var connection = mysql.createConnection(stuff.parsed);
-console.log(stuff.parsed);
+
+//dotenv package is hide the credential
+require('dotenv').config()
+
+//hiding the credential
+var connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+});
 
 connection.connect();
 
-connection.query('SELECT * FROM products', function (error, results, fields) {
+//Establishing connections
+connection.query('SELECT item_id, product_name, department_name, price FROM products', function (error, results, fields) {
     if (error) throw error;
-    console.log(results);
+    console.table(results);
+    
 });
 
-connection.end();
+
+
+
+
+// connection.end();
