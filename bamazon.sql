b489c14360b474993ae734faed0d34229ee83613
miko <ptechasa@mail.ccsf.edu>
2019-09-22 01:19:20 -0700

    fixed on checking item_id condition

--- bamazon.sql
+++ bamazon.sql
@@ -4,7 +4,7 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products (
-item_id  INT NOT NULL AUTO_INCREMENT,
+item_id  INT NOT NULL,
 product_name VARCHAR(255),
 department_name VARCHAR(255),
 price DECIMAL (8,2), 
@@ -20,6 +20,6 @@ INSERT INTO products (item_id, product_name, department_name, price, stock_quant
 ('5005', 'Kate Spade New York', 'Watches', '195', '25'),
 ('6006', 'PRADA', 'Sunglasses', '295.99', '6'),
 ('7007', 'NIKE Zoom Vapor Cage 3', 'New Releases', '199.99', '10'),
-('8001', 'NIKE Kids Air MAx', 'Performance', '70', '30'),
+('800', 'NIKE Kids Air MAx', 'Performance', '70', '30'),
 ('9009', 'Columbia Jacket', 'Outdoor', '299', '100'),
 ('1111', 'Bamazon Gift CArds', 'Gift Cards', '100', '100');
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,7 +8,7 @@ const keys = require('./keys.js');
 //dotenv package is hide the credential
 require('dotenv').config()
 
-
+//Establishing connections
 var connection = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
@@ -18,14 +18,15 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-//Establishing connections
-connection.query('SELECT item_id, product_name, department_name, price FROM products', function (error, results, fields) {
-    if (error) throw error;
-    console.table(results);
-    askId();
-});
+function viewProducts() {
+    connection.query('SELECT item_id AS ID, product_name AS Product, department_name AS Department, price AS Price, stock_quantity AS Quantity FROM products', function (error, results, fields) {
+        if (error) throw error;
+        console.table(results);
+        askProductId();
+    });
+}
 
-function askId() {
+function askProductId() {
     inquirer
         .prompt([
             {
@@ -37,17 +38,24 @@ function askId() {
         .then(answers => {
             var numID = parseInt(answers.id)
 
-            if (numID === 1001 || 2002 || 3003 || 4004
-                || 5005 || 6006 || 7007 || 8008 || 9009 || 1111) {
-                // var id = answers.id
-                // console.log(id)
+            //check item_id on Database 
+            if (numID == 1001 ||
+                numID == 2002 ||
+                numID == 3003 ||
+                numID == 4004 ||
+                numID == 5005 ||
+                numID == 6006 ||
+                numID == 7007 ||
+                numID == 8008 ||
+                numID == 9009 ||
+                numID == 1111) {
+
                 askQty(numID);
 
             } else {
-                console.log('Please select correct ID #')
+                console.log('Please input correct Product ID, try again!!')
+                askProductId();
             }
-
-
         });
 }
 
@@ -63,32 +71,51 @@ function askQty(num) {
         .then(answers => {
             var qty = parseInt(answers.qty)
 
+            //using ? to avoid a SQL injection attack
             connection.query('SELECT * FROM products WHERE item_id = ?', [num], function (error, results, fields) {
+
                 if (error) throw error;
+
                 var dbQty = results[0].stock_quantity;
                 var priceNum = parseInt(results[0].price)
 
                 if (qty > dbQty) {
+                    console.log('----------------------------')
                     console.log('Insufficient quantity!')
 
                     //if insufficient quantity, it will show the product_name and stock_quantity that you have in stock
-                    connection.query('SELECT product_name, stock_quantity FROM products WHERE item_id = ?', [num], function (error, results, fields) {
+                    connection.query('SELECT product_name AS Product, stock_quantity AS Quantity FROM products WHERE item_id = ?', [num], function (error, results, fields) {
                         console.table(results);
+                        console.log(`Total stock that we have: ${dbQty}`)
+                        askProductId();
                     })
 
                 } else {
                     var totalPrice = qty * priceNum
+                    console.log('----------------------------')
                     console.log(`${qty} items have been ordered.`)
                     console.log(`${results[0].product_name} at $${results[0].price}`)
+                    console.log('----------------------------')
                     console.log(`Total amount: $${totalPrice}`)
+                    console.log('============================')
+                    // console.log('---Thank you for shopping at Bamazon ---')
+                    askProductId();
                 }
                 // console.log(results[0].stock_quantity);
-                // askId();
+
 
             });
         });
 }
 
+function updateInventory(num) {
+    connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [num], function (error, results, fields) {
+        if (error) throw error;
+        console.table(results);
+    });
+}
+
+viewProducts();
 
 //Closing the connection
 // connection.end();
